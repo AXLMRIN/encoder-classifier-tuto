49fed3297a40abf05efc8cec61b447b1d48721ac
A.Morin <axelmorinmail@gmail.com>
2026-03-26 17:53:59 +0100

    adapt to nlabel

--- tests-multilabel/routine-multilabel.py
+++ tests-multilabel/routine-multilabel.py
@@ -1,7 +1,10 @@
+import os 
+
 from datasets import load_from_disk
 
 from src.functions import clean_memory
 from src.classification_pipe import ClassificationPipe
+from src.prediciton_pipe import PredictionPipe
 
 MODEL_NAME = "google-bert/bert-base-uncased"
 MAX_LENGTH = 512
@@ -16,33 +19,52 @@ CLASSES = [
     'brain dysfunction (forget)', 
     'anger'
 ]
-DEVICE = "cpu"
-DEVICE_BATCH_SIZE = 4
+DEVICE = "cuda"
+DEVICE_BATCH_SIZE = 8
+TEXT_COLUMN = "text-clean-no-emoji"
 
-dsd = load_from_disk("./data/multi-label-split/")
-print(dsd)
-pipe = None
-try: 
+
+pipe, dsd = None, None
+for n_classes in range(2, len(CLASSES)): 
+    output_dir = f"./models/multilabel-top-{n_classes}"
+
+    dsd = load_from_disk("./data/multi-label-split/")
     pipe = ClassificationPipe(
         mode = "multilabel",
-        text_column = "text-clean-no-emoji",
-        label_columns = CLASSES[:3],
-        output_dir = "./models/test-multilabel",
+        text_column = TEXT_COLUMN,
+        label_columns = CLASSES[:n_classes],
+        output_dir = output_dir,
         model_name = MODEL_NAME, 
         device_batch_size = DEVICE_BATCH_SIZE, 
         device = DEVICE, 
         tokenizer_max_length = MAX_LENGTH,
-        additional_training_arguments={"num_train_epochs" : 2}
+        additional_training_arguments={
+            "num_train_epochs" : 5,
+            "gradient_accumulation_steps" : 2,
+            "fp16" : True,
+        }
     )
     print(pipe)
     dsd = pipe.tokenize(dsd)
 
-    pipe.train(dsd, test_mode = True)
-    pipe.save_important_info("./models/config/test-multilabel.json")
+    pipe.train(dsd)
+    config = pipe.save_important_info(f"./model-configs/multilabel-top-{n_classes}.json")
 
-except Exception as e:
-    print(f"ERROR IN ROUTINE\n{e}")
-finally:
-    del pipe, dsd
-    clean_memory()
-    
+    for checkpoint in os.listdir(output_dir):
+        pipe_pred = PredictionPipe(
+            checkpoint_dir=f"{output_dir}/{checkpoint}",
+            model_name = MODEL_NAME,
+            text_column = TEXT_COLUMN, 
+            labels = [True, False],
+            tokenizer_parameters = config["tokenizer_parameters"],
+            device_batch_size = DEVICE_BATCH_SIZE, 
+            device = DEVICE
+        )
+        predictions = pipe_pred.predict(dsd)
+        for split in predictions:
+            predictions[split].to_csv(f"./outputs/multilabel-top-{n_classes}-{split}.csv")
+        os.system(f"rm -rf {output_dir}/{checkpoint}")
+
+
+del pipe, dsd
+clean_memory()
